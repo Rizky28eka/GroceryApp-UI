240626a07ba746698b4da08003f2b44eedefcacd
Rizky Eka Haryadi <127827977+rizky-eka@users.noreply.github.com>
2023-11-28 06:17:37 +0700

    Update Header | HomePage

--- lib/pages/home/components/header.dart
+++ lib/pages/home/components/header.dart
@@ -24,7 +24,7 @@ class HomeHeader extends StatelessWidget {
                 style: Theme.of(context).textTheme.caption,
               ),
               Text(
-                "Alex Redrigson",
+                "Alexis Comparison",
                 style: Theme.of(context)
                     .textTheme
                     .subtitle1!
